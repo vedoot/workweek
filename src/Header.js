5cc7ed4f59fccee423ffe0cea321bd7c39882ee9
Vedant <vm.WorkAddress@gmail.com>
2020-02-02 17:36:17 -0500

    Changed ids to Class

--- src/Header.js
+++ src/Header.js
@@ -26,14 +26,14 @@ class Header extends React.Component{
 
   render(){
     return(
-      <div key={1} id="upperContainer">
-      <div id="headline">
+      <div key={1} className="upperContainer">
+      <div className="headline">
         <p>This is what your<br/> week looks like.</p>
-        <p id="tagline"> Better get to work</p>
+        <p className="tagline"> Better get to work</p>
       </div>
-      <div id="linkContainer">
-        <a target="_blank" rel="noopener noreferrer" href="http://calendar.google.com/" id="today"> {this.state.date.toString()}</a>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/VMehta99/workweek" id="instructions">Some Instructions</a>
+      <div className="linkContainer">
+        <a target="_blank" rel="noopener noreferrer" href="http://calendar.google.com/" className="today"> {this.state.date.toString()}</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/VMehta99/workweek" className="instructions">Some Instructions</a>
        </div>
     </div>
     )
