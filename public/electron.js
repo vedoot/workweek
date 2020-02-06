2e2b2f65c95d3409d60911b4d25787338c76c575
Vedant <vm.WorkAddress@gmail.com>
2020-02-06 17:49:07 -0500

    electron babyyy

---
+++ public/electron.js
@@ -0,0 +1,67 @@
+const electron = require('electron');
+const { app, globalShortcut,Menu,shell } = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow.setFullScreen(true);
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', ()=>{
+  createWindow();
+
+  globalShortcut.register('Cmd+Shift+9', () => {
+    //this will get call for Control+Shift+I.
+    console.log("dev tools is now open");
+    mainWindow.webContents.openDevTools()
+  });
+  var template = [{
+    label: "WorkWeek",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]},
+    {
+      label:"👍🏽",
+      submenu:[
+        {label: "instructions (for the weak)", click:function(){shell.openExternal('https://github.com/VMehta99/workweek');}}
+      ]
+    }
+    
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
